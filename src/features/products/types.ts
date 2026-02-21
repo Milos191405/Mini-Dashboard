1c585dd1d64df822aca2a1050ec583d6823ef731
Milos191405 <milos.mirkovic7@gmail.com>
2026-02-21 17:02:33 +0100

    feat(state): add redux store and products slice (products thunks & types)

---
+++ src/features/products/types.ts
@@ -0,0 +1,53 @@
+// Products list
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  category: string;
+  brand: string;
+  thumbnail: string;
+  images: string[];
+}
+
+// Products Details
+
+export interface ProductDetails extends Product {
+  tags?: string[];
+  sku?: string;
+  weight?: number;
+  dimensions?: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  warrantyInformation?: string;
+  shippingInformation?: string;
+  availabilityStatus?: string;
+  reviews?: {
+    rating: number;
+    comment: string;
+    date: string;
+    reviewerName: string;
+    reviewerEmail: string;
+  }[];
+  returnPolicy?: string;
+  minimumOrderQuantity?: number;
+  meta?: {
+    createdAt: string;
+    updatedAt: string;
+    barcode: string;
+    qrCode?: string;
+  };
+}
+
+export interface ProductState {
+  products: Product[];
+  selectedProduct?: ProductDetails;
+  status: "idle" | "loading" | "succeeded" | "failed";
+  error: string | null;
+}
